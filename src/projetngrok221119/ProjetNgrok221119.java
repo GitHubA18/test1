898a3f5d637e67a1aa0fa396506d70501a7a99da
GitHubA18 <56780224+GitHubA18@users.noreply.github.com>
2019-11-22 16:09:23 -0500

    Commit v2

--- src/projetngrok221119/ProjetNgrok221119.java
+++ src/projetngrok221119/ProjetNgrok221119.java
@@ -16,6 +16,7 @@ public class ProjetNgrok221119 {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour" + args[1] +", "+args[0]);
+        System.out.println("Au revoir !");
     }
     
 }
