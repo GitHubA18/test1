f1b78519cc6aebcc11c1ac5919139782912ebaa3
GitHubA18 <56780224+GitHubA18@users.noreply.github.com>
2019-11-22 13:54:49 -0500

    Commit du Projet

---
+++ src/projetngrok221119/ProjetNgrok221119.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetngrok221119;
+
+/**
+ *
+ * @author User
+ */
+public class ProjetNgrok221119 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour" + args[1] +", "+args[0]);
+    }
+    
+}
